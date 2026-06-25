18e2fd04f12ba8cc6f05f001fe2d359e572b1773
fabriciof807 <fabriciof807@proton.me>
2026-06-25 15:52:19 -0300

    chore: bump version to v2.4.1 (versionCode 15)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.novelreader"
         minSdk = 26
         targetSdk = 34
-        versionCode = 14
-        versionName = "2.4.0"
+        versionCode = 15
+        versionName = "2.4.1"
 
         buildConfigField("String", "MVLEMPYR_API_URL", "\"https://chap.heliosarchive.online/wp-json/wp/v2/mvl-characters?per_page=15000\"")
     }
